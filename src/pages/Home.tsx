d7c082f388381032c12df9d59b4bd16131358966
Abe Ahn <abemsq@gmail.com>
2023-05-23 20:36:45 +0900

    changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,13 @@ import '../styles/Home.css';
 import db from '../data/db.json';
 
 const Home: React.FC = () => {
+//  const [filters, setFilters] = useState<{ [key: string]: string[] }>({});
   const [carList, setCarList] = useState<CarData[]>([]);
   
+  const handleFilterChange = (filterName: string, selectedItems: string[]) => {
+ //   setFilters((prevFilters: any) => ({ ...prevFilters, [filterName]: selectedItems }));
+  }
+
   useEffect(() => {
     setCarList(db.carClasses);
   }, []);
